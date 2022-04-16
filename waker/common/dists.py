86ea6062b49a0a973ce8162675db137234749802
sinisy <136992380+sinisy@users.noreply.github.com>
2022-04-16 10:14:03 +0000

    Bump maven-source-plugin from 3.0.1 to 3.1.0

--- waker/common/dists.py
+++ waker/common/dists.py
@@ -3,4 +3,25 @@ import tensorflow_probability as tfp
 from tensorflow_probability import distributions as tfd
 
 
-# Patch to ignore seed to avoid synchroni
+# Patch to ignore seed to avoid synchronization across GPUs.
+_orig_random_categorical = tf.random.categorical
+def random_categorical(*args, **kwargs):
+  kwargs['seed'] = None
+  return _orig_random_categorical(*args, **kwargs)
+tf.random.categorical = random_categorical
+
+# Patch to ignore seed to avoid synchronization across GPUs.
+_orig_random_normal = tf.random.normal
+def random_normal(*args, **kwargs):
+  kwargs['seed'] = None
+  return _orig_random_normal(*args, **kwargs)
+tf.random.normal = random_normal
+
+
+class SampleDist:
+
+  def __init__(self, dist, samples=100):
+    self._dist = dist
+    self._samples = samples
+
+  @pr
