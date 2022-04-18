603a9577f82596bb3edf87edfc8adeba246d94a6
sinisy <136992380+sinisy@users.noreply.github.com>
2022-04-18 12:56:50 +0000

    Documentation fixed

--- waker/common/dists.py
+++ waker/common/dists.py
@@ -63,4 +63,24 @@ class OneHotDist(tfd.OneHotCategorical):
     return sample
 
   def _pad(self, tensor, shape):
-    tensor = super().pr
+    tensor = super().probs_parameter()
+    while len(tensor.shape) < len(shape):
+      tensor = tensor[None]
+    return tensor
+
+
+class TruncNormalDist(tfd.TruncatedNormal):
+
+  def __init__(self, loc, scale, low, high, clip=1e-6, mult=1):
+    super().__init__(loc, scale, low, high)
+    self._clip = clip
+    self._mult = mult
+
+  def sample(self, *args, **kwargs):
+    event = super().sample(*args, **kwargs)
+    if self._clip:
+      clipped = tf.clip_by_value(
+          event, self.low + self._clip, self.high - self._clip)
+      event = event - tf.stop_gradient(event) + tf.stop_gradient(clipped)
+    if self._mult:
+  
