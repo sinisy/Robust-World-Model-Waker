aa15d7488abfbb573ba4200a64d07227083bad0f
sinisy <136992380+sinisy@users.noreply.github.com>
2022-04-21 17:47:50 +0000

    ALSA: hda - Add a missing quirk entry for iMac 9,1

--- waker/common/driver.py
+++ waker/common/driver.py
@@ -16,4 +16,24 @@ class Driver:
     self.reset()
 
   def on_step(self, callback):
- 
+    self._on_steps.append(callback)
+
+  def on_reset(self, callback):
+    self._on_resets.append(callback)
+
+  def on_episode(self, callback):
+    self._on_episodes.append(callback)
+
+  def on_call(self, callback):
+    self._on_calls.append(callback)
+
+  def reset(self):
+    self._obs = [None] * len(self._envs)
+    self._eps = [None] * len(self._envs)
+    self._state = None
+
+  def __call__(self, policy, steps=0, episodes=0, env_sampler=None, task=None):
+    step, episode = 0, 0
+    eps = []
+    while step < steps or episode < episodes:
+      if env_sampler is None:
