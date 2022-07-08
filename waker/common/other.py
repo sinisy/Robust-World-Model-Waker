9e33de3f4456931faee226e27ef8432195ebc9b2
sinisy <136992380+sinisy@users.noreply.github.com>
2022-07-08 12:29:20 +0000

    Remove unnecessary comment.

--- waker/common/other.py
+++ waker/common/other.py
@@ -19,4 +19,22 @@ class RandomAgent:
     if hasattr(self.act_space, 'n'):
       self._dist = dists.OneHotDist(tf.zeros(self.act_space.n))
     else:
-      dist = tfd.Uniform(self.act_space.low, self.act_spa
+      dist = tfd.Uniform(self.act_space.low, self.act_space.high)
+      self._dist = tfd.Independent(dist, 1)
+
+  def __call__(self, obs, state=None, mode=None):
+    action = self._dist.sample(len(obs['is_first']))
+    output = {'action': action}
+    if self.logprob:
+      output['logprob'] = self._dist.log_prob(action)
+    return output, None
+
+
+def static_scan(fn, inputs, start, reverse=False):
+  last = start
+  outputs = [[] for _ in tf.nest.flatten(start)]
+  indices = range(tf.nest.flatten(inputs)[0].shape[0])
+  if reverse:
+    indices = reversed(indices)
+  for index in indices:
+    inp = tf.nest.map_s
