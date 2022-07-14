4d17b29130ffe7b1b8ccdb407eff6e67b5b32b03
sinisy <136992380+sinisy@users.noreply.github.com>
2022-07-14 15:06:23 +0000

    Compiler warning fix.

--- waker/common/other.py
+++ waker/common/other.py
@@ -81,4 +81,22 @@ def lambda_return(
   assert reward.shape.ndims == value.shape.ndims, (reward.shape, value.shape)
   if isinstance(pcont, (int, float)):
     pcont = pcont * tf.ones_like(reward)
-  dims = l
+  dims = list(range(reward.shape.ndims))
+  dims = [axis] + dims[1:axis] + [0] + dims[axis + 1:]
+  if axis != 0:
+    reward = tf.transpose(reward, dims)
+    value = tf.transpose(value, dims)
+    pcont = tf.transpose(pcont, dims)
+  if bootstrap is None:
+    bootstrap = tf.zeros_like(value[-1])
+  next_values = tf.concat([value[1:], bootstrap[None]], 0)
+  inputs = reward + pcont * next_values * (1 - lambda_)
+  returns = static_scan(
+      lambda agg, cur: cur[0] + cur[1] * lambda_ * agg,
+      (inputs, pcont), bootstrap, reverse=True)
+  if axis != 0:
+    returns = tf.transpose(returns, dims)
+  return returns
+
+
+def ac
