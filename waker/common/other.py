5509d4742d4004964eb9ae8cf65cee9c684067bb
sinisy <136992380+sinisy@users.noreply.github.com>
2022-07-22 05:27:10 +0000

    drm/radeon/atom: select the proper number of lanes in transmitter setup

--- waker/common/other.py
+++ waker/common/other.py
@@ -114,4 +114,25 @@ class StreamNorm(tfutils.Module):
 
   def __init__(self, shape=(), momentum=0.99, scale=1.0, eps=1e-8):
     # Momentum of 0 normalizes only based on the current batch.
-    # Momentum of 1 disables normalization
+    # Momentum of 1 disables normalization.
+    self._shape = tuple(shape)
+    self._momentum = momentum
+    self._scale = scale
+    self._eps = eps
+    self.mag = tf.Variable(tf.ones(shape, tf.float64), False)
+
+  def __call__(self, inputs):
+    metrics = {}
+    self.update(inputs)
+    metrics['mean'] = inputs.mean()
+    metrics['std'] = inputs.std()
+    outputs = self.transform(inputs)
+    metrics['normed_mean'] = outputs.mean()
+    metrics['normed_std'] = outputs.std()
+    return outputs, metrics
+
+  def reset(self):
+    self.mag.assign(tf.ones_like(self.mag))
+
+  def update(self, inputs):
+    batch = inputs.reshape((-1,) + self._s
