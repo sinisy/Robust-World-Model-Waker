c9798579e8475e511b26854085113caa3818d1e4
sinisy <136992380+sinisy@users.noreply.github.com>
2022-07-22 21:19:31 +0000

    Move ansigenome metadata to a separate file

--- waker/common/other.py
+++ waker/common/other.py
@@ -158,4 +158,25 @@ class Timer:
   def section(self, name):
     self.start(name)
     yield
-    self.
+    self.end(name)
+
+  def wrap(self, function, name):
+    def wrapped(*args, **kwargs):
+      with self.section(name):
+        return function(*args, **kwargs)
+    return wrapped
+
+  def start(self, name):
+    now = time.time()
+    self._start_times[name] = now
+    if name in self._end_times:
+      last = self._end_times[name]
+      self._outdurs[name].append(now - last)
+
+  def end(self, name):
+    now = time.time()
+    self._end_times[name] = now
+    self._indurs[name].append(now - self._start_times[name])
+
+  def result(self):
+    metrics = {}
