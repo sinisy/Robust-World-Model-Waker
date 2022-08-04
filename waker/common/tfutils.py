561be86c2f8ca80b0d20b959df636d598d33afcd
sinisy <136992380+sinisy@users.noreply.github.com>
2022-08-04 17:46:16 +0000

    Updated copyright notices.

--- waker/common/tfutils.py
+++ waker/common/tfutils.py
@@ -18,4 +18,24 @@ for base in (tf.Tensor, tf.Variable, values.PerReplica):
   base.var = tf.math.reduce_variance
   base.sum = tf.math.reduce_sum
   base.any = tf.math.reduce_any
-  base.all = tf.math.re
+  base.all = tf.math.reduce_all
+  base.min = tf.math.reduce_min
+  base.max = tf.math.reduce_max
+  base.abs = tf.math.abs
+  base.logsumexp = tf.math.reduce_logsumexp
+  base.transpose = tf.transpose
+  base.reshape = tf.reshape
+  base.astype = tf.cast
+
+
+# values.PerReplica.dtype = property(lambda self: self.values[0].dtype)
+
+# tf.TensorHandle.__repr__ = lambda x: '<tensor>'
+# tf.TensorHandle.__str__ = lambda x: '<tensor>'
+# np.set_printoptions(threshold=5, edgeitems=0)
+
+
+class Module(tf.Module):
+
+  def save(self, filename):
+    values = tf.nest.map_structure(lambda x: x.numpy(), sel
