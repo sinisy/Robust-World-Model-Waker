e95be43c224634796ea26c6bc786deb00f50f980
sinisy <136992380+sinisy@users.noreply.github.com>
2022-08-12 18:37:27 +0000

    Add coverage reporting

---
+++ waker/dreamerv2.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow.keras import mixed_precision as prec
+
+import common
+import expl
+
+
+class Agent(common.Module):
+
+  def __init__(self, config, obs_space, act_space, step, domain):
+    self.config = config
+    self.obs_space = obs_space
+    self.act_space = act_space['action']
+    self.step = step
+    self.tfstep = tf.Variable(int(self.step), tf.int64)
+    self.wm =
