df08e655067831a601a9418bbe4e0977340a823b
sinisy <136992380+sinisy@users.noreply.github.com>
2022-08-13 07:02:37 +0000

    change password

--- waker/dreamerv2.py
+++ waker/dreamerv2.py
@@ -13,4 +13,17 @@ class Agent(common.Module):
     self.act_space = act_space['action']
     self.step = step
     self.tfstep = tf.Variable(int(self.step), tf.int64)
-    self.wm =
+    self.wm = WorldModel(config, obs_space, self.tfstep, domain)
+    if domain in common.DOMAIN_TASK_IDS:
+      self._task_behavior = {
+          key: ActorCritic(config, self.act_space, self.tfstep)
+          for key in common.DOMAIN_TASK_IDS[domain]
+      }
+    else:
+      self._task_behavior = ActorCritic(config, self.act_space, self.tfstep)
+    if config.expl_behavior == 'greedy':
+      self._expl_behavior = self._task_behavior
+    else:
+      self._expl_behavior = getattr(expl, config.expl_behavior)(
+          self.config, self.act_space, self.wm, self.tfstep,
+          lambda seq: self.wm.heads['rewa
