ead798360bacb8c1db7218ffc38d799d8e0ba6c6
sinisy <136992380+sinisy@users.noreply.github.com>
2022-10-28 14:09:13 +0000

    nilfs2: fix issue with race condition of competition between segments for dirty blocks

--- waker/envs/dm_control/mujoco/index.py
+++ waker/envs/dm_control/mujoco/index.py
@@ -8,4 +8,21 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  i
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Mujoco functions to support named indexing.
+
+The Mujoco name structure works as follows:
+
+In mjxmacro.h, each "X" entry denotes a type (a), a field name (b) and a list
+of dimension size metadata (c) which may contain both numbers and names, for
+example
+
+   X(int,    name_bodyadr, nbody, 1) // or
+   X(mjtNum, body_pos,     nbody, 3)
+     a       b             c ----->
+
+The sec
