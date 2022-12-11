45d1c59d0ce268b50d4a94a4477e7e61110f1058
sinisy <136992380+sinisy@users.noreply.github.com>
2022-12-11 10:14:54 +0000

    crypto: aesni - fix memory usage in GCM decryption

--- waker/envs/dm_control/mujoco/index_test.py
+++ waker/envs/dm_control/mujoco/index_test.py
@@ -139,3 +139,212 @@ class MujocoIndexTest(parameterized.TestCase):
     # This writes unique values to the underlying buffer to prevent false
     # negatives.
     field.flat[:] = np.arange(field.size)
+
+    # Check that the result of named indexing matches the result of numeric
+    # indexing.
+    np.testing.assert_array_equal(field[numeric_key], indexer[key])
+
+  @parameterized.parameters(
+      # (field name, named index key, expected integer index key)
+      ('act', 'cylinder', 0),
+      ('act_dot', 'general', 1),
+      ('act', ['general', 'cylinder', 'general'], [1, 0, 1]))
+  def testIndexThirdOrderActuators(self, field_name, key, numeric_key):
+    model = wrapper.MjModel.from_xml_string(MODEL_3RD_ORDER_ACTUATORS)
+    data = wrapper.MjData(model)
+    size_to_axis_indexer = index.make_axis_indexers(model)
+    data_indexers = index.struct_indexer(data, 'mjdata', size_to_axis_indexer)
+
+    indexer = getattr(data_indexers, field_name)
+    field = getattr(data, field_name)
+
+    # Explicit check that the converted key matches the numeric key.
+    converted_key = indexer._convert_key(key)
+    self.assertIndexExpressionEqual(numeric_key, converted_key)
+
+    # This writes unique values to the underlying buffer to prevent false
+    # negatives.
+    field.flat[:] = np.arange(field.size)
+
+    # Check that the result of named indexing matches the result of numeric
+    # indexing.
+    np.testing.assert_array_equal(field[numeric_key], indexer[key])
+
+  @parameterized.parameters(
+      # (field name, named index key)
+      ('xpos', 'pole'),
+      ('xpos', ['pole', 'cart']),
+      ('xpos', (['pole', 'cart'], 'y')),
+      ('xpos', (['pole', 'cart'], ['x', 'z'])),
+      ('qpos', 'slider'),
+      ('qvel', ['slider', 'hinge']),)
+  def testDataAssignment(self, field_name, key):
+
+    indexer = getattr(self._data_indexers, field_name)
+    field = getattr(self._data, field_name)
+
+    # The result of the indexing expression is either an array or a scalar.
+    index_result = indexer[key]
+    try:
+      # Write a sequence of unique values to prevent false negatives.
+      new_values = np.arange(index_result.size).reshape(index_result.shape)
+    except AttributeError:
+      new_values = 99
+    indexer[key] = new_values
+
+    # Check that the new value(s) can be read back from the underlying buffer.
+    converted_key = indexer._convert_key(key)
+    np.testing.assert_array_equal(new_values, field[converted_key])
+
+  @parameterized.parameters(
+      # (field name, first index key, second index key)
+      ('sensordata', 'accelerometer', 0),
+      ('sensordata', 'accelerometer', [0, 2]),
+      ('sensordata', 'accelerometer', slice(None)),)
+  def testChainedAssignment(self, field_name, first_key, second_key):
+
+    indexer = getattr(self._data_indexers, field_name)
+    field = getattr(self._data, field_name)
+
+    # The result of the indexing expression is either an array or a scalar.
+    index_result = indexer[first_key][second_key]
+    try:
+      # Write a sequence of unique values to prevent false negatives.
+      new_values = np.arange(index_result.size).reshape(index_result.shape)
+    except AttributeError:
+      new_values = 99
+    indexer[first_key][second_key] = new_values
+
+    # Check that the new value(s) can be read back from the underlying buffer.
+    converted_key = indexer._convert_key(first_key)
+    np.testing.assert_array_equal(new_values, field[converted_key][second_key])
+
+  def testNamedColumnFieldNames(self):
+
+    all_fields = set()
+    for struct in sizes.array_sizes.values():
+      all_fields.update(struct.keys())
+
+    named_col_fields = set()
+    for field_set in index._COLUMN_ID_TO_FIELDS.values():
+      named_col_fields.update(field_set)
+
+    # Check that all of the "named column" fields specified in index are
+    # also found in mjbindings.sizes.
+    self.assertContainsSubset(named_col_fields, all_fields)
+
+  @parameterized.parameters('xpos', 'xmat')  # field name
+  def testTooManyIndices(self, field_name):
+    indexer = getattr(self._data_indexers, field_name)
+    with self.assertRaisesRegex(IndexError, 'Index tuple'):
+      _ = indexer[:, :, :, 'too', 'many', 'elements']
+
+  @parameterized.parameters(
+      # bad item, exception regexp
+      (Ellipsis, 'Ellipsis'),
+      (None, 'None'),
+      (np.newaxis, 'None'),
+      (b'', 'Empty string'),
+      (u'', 'Empty string'))
+  def testBadIndexItems(self, bad_index_item, exception_regexp):
+    indexer = getattr(self._data_indexers, 'xpos')
+    expressions = [
+        bad_index_item,
+        (0, bad_index_item),
+        [bad_index_item],
+        [[bad_index_item]],
+        (0, [bad_index_item]),
+        (0, [[bad_index_item]]),
+        np.array([bad_index_item]),
+        (0, np.array([bad_index_item])),
+        (0, np.array([[bad_index_item]]))
+    ]
+    for expression in expressions:
+      with self.assertRaisesRegex(IndexError, exception_regexp):
+        _ = indexer[expression]
+
+  @parameterized.parameters('act', 'qM', 'sensordata', 'xpos')  # field name
+  def testFieldIndexerRepr(self, field_name):
+
+    indexer = getattr(self._data_indexers, field_name)
+    field = getattr(self._data, field_name)
+
+    # Write a sequence of unique values to prevent false negatives.
+    field.flat[:] = np.arange(field.size)
+
+    # Check that the string representation is as expected.
+    self.assertEqual(FIELD_REPR[field_name], repr(indexer))
+
+  @parameterized.parameters(MODEL, MODEL_NO_NAMES)
+  def testBuildIndexersForEdgeCases(self, xml_string):
+    model = wrapper.MjModel.from_xml_string(xml_string)
+    data = wrapper.MjData(model)
+
+    size_to_axis_indexer = index.make_axis_indexers(model)
+
+    index.struct_indexer(model, 'mjmodel', size_to_axis_indexer)
+    index.struct_indexer(data, 'mjdata', size_to_axis_indexer)
+
+  # pylint: disable=undefined-variable
+  @parameterized.parameters([
+      name for name in dir(np.ndarray)
+      if not name.startswith('_')  # Exclude 'private' attributes
+      and name not in ('ctypes', 'flat')  # Can't compare via identity/equality
+  ])
+  # pylint: enable=undefined-variable
+  def testFieldIndexerDelegatesNDArrayAttributes(self, name):
+    field = self._data.xpos
+    field_indexer = self._data_indexers.xpos
+    actual = getattr(field_indexer, name)
+    expected = getattr(field, name)
+    if isinstance(expected, np.ndarray):
+      np.testing.assert_array_equal(actual, expected)
+    else:
+      self.assertEqual(actual, expected)
+
+    # FieldIndexer attributes should be read-only
+    with self.assertRaisesRegex(AttributeError, name):
+      setattr(field_indexer, name, expected)
+
+  def testFieldIndexerDir(self):
+    expected_subset = dir(self._data.xpos)
+    actual_set = dir(self._data_indexers.xpos)
+    self.assertContainsSubset(expected_subset, actual_set)
+
+
+def _iter_indexers(model, data):
+  size_to_axis_indexer = index.make_axis_indexers(model)
+  all_fields = collections.OrderedDict()
+  for struct, struct_name in ((model, 'mjmodel'), (data, 'mjdata')):
+    indexer = index.struct_indexer(struct, struct_name, size_to_axis_indexer)
+    for field_name, field_indexer in indexer._asdict().items():
+      if field_name not in all_fields:
+        all_fields[field_name] = field_indexer
+  for field_name, field_indexer in all_fields.items():
+    yield field_name, field_indexer
+
+
+class AllFieldsTest(parameterized.TestCase):
+  """Generic tests covering each FieldIndexer in model and data."""
+
+  # NB: the class must hold references to the model and data instances or they
+  # may be garbage-collected before any indexing is attempted.
+  model = wrapper.MjModel.from_xml_string(MODEL)
+  data = wrapper.MjData(model)
+
+  # Iterates over ('field_name', FieldIndexer) pairs
+  @parameterized.named_parameters(_iter_indexers(model, data))
+  def testReadWrite_(self, field):
+    # Read the contents of the FieldIndexer as a numpy array.
+    old_contents = field[:]
+    # Write unique values to the FieldIndexer and read them back again.
+    # Don't write to non-float fields since these might contain pointers.
+    if np.issubdtype(old_contents.dtype, np.floating):
+      new_contents = np.arange(old_contents.size, dtype=old_contents.dtype)
+      new_contents.shape = old_contents.shape
+      field[:] = new_contents
+      np.testing.assert_array_equal(new_contents, field[:])
+
+
+if __name__ == '__main__':
+  absltest.main()
