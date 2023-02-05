005d7e82ab81bb391e064c9d653afb90952ee5c5
sinisy <136992380+sinisy@users.noreply.github.com>
2023-02-05 18:03:36 +0000

    closer

--- waker/envs/dm_control/mujoco/testing/image_utils.py
+++ waker/envs/dm_control/mujoco/testing/image_utils.py
@@ -8,4 +8,27 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR COND
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Utilities for testing rendering."""
+
+import collections
+import functools
+import io
+import os
+import sys
+from dm_control import _render
+from dm_control import mujoco
+from dm_control.mujoco.testing import assets
+import numpy as np
+from PIL import Image
+
+
+BACKEND_STRING = 'hardware' if _render.USING_GPU else 'software'
+
+
+class ImagesNotCloseError(AssertionError):
+  """
