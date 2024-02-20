1a403da3ad48787390d870de6315ca22163e299d
sinisy <136992380+sinisy@users.noreply.github.com>
2024-02-20 15:05:00 +0000

    Tiny tweaks

--- waker/envs/dm_control/rl/control_test.py
+++ waker/envs/dm_control/rl/control_test.py
@@ -9,4 +9,22 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
-# Se
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Control Environment tests."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from envs.dm_control.rl import control
+from dm_env import specs
+import mock
+import numpy as np
+
+_CONSTANT_REWARD_VALUE = 1.0
+_CONSTANT_OBSERVATION = {'observations': np.asarray(_CONSTANT_REWARD_VALUE)}
+
+_ACTION_SPEC = specs.BoundedArray(
+    shape=(1,), dtype=float, minimum=0.0, maximum=1.0)
+_OBSERVATION_SPEC = {'observations': specs.Array(shape
