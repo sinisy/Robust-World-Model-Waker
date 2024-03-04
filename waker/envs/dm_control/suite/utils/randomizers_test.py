e498de7882e4653f8348a8ce437385211c446384
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-04 15:34:44 +0000

    Update archinstall003

--- waker/envs/dm_control/suite/utils/randomizers_test.py
+++ waker/envs/dm_control/suite/utils/randomizers_test.py
@@ -9,4 +9,27 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
-# See the License for the specific language governing permissions an
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Tests for randomizers.py."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from dm_control import mujoco
+from dm_control.mujoco.wrapper import mjbindings
+from envs.dm_control.suite.utils import randomizers
+import numpy as np
+
+mjlib = mjbindings.mjlib
+
+
+class RandomizeUnlimitedJointsTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.rand = np.random.RandomState(100)
+
+  def test_single_joint_of_each_type(self):
+    physic
