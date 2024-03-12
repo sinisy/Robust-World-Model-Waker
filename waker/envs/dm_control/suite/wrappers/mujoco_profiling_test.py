946356338b7535664bc765c6a97c9a2be74f5f81
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-12 11:12:11 +0000

    Cleaned up source code

--- waker/envs/dm_control/suite/wrappers/mujoco_profiling_test.py
+++ waker/envs/dm_control/suite/wrappers/mujoco_profiling_test.py
@@ -11,4 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ====
+# ============================================================================
+"""Tests for the mujoco_profiling wrapper."""
+
+import collections
+
+from absl.testing import absltest
+from envs.dm_control.suite import cartpole
+from envs.dm_control.suite.wrappers import mujoco_profiling
+import numpy as np
+
+
+class MujocoProfilingTest(absltest.TestCase):
+
+  def test_dict_observation(self):
+    obs_key = 'mjprofile'
+
+    env = cartpole.swingup()
+
+    # Make sure we are testing the right environment for the test.
+    observation_spec = env.observation_spec()
+    self.assertIsInstance(observa
