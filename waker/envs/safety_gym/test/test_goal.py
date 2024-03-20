a7e8903be15a1cf556895ae26a2c36fb53fb4b3c
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-20 06:19:12 +0000

    fib_rules: fix fib rule dumps across multiple skbs

--- waker/envs/safety_gym/test/test_goal.py
+++ waker/envs/safety_gym/test/test_goal.py
@@ -14,3 +14,20 @@ class TestGoal(unittest.TestCase):
             _, _, done, _ = env.step([1,0])
 
     def test_resample(self):
+        ''' Episode should end with resampling failure '''
+        config = {
+            'robot_base': 'xmls/point.xml',
+            'num_steps': 1001,
+            'placements_extents': [-1, -1, 1, 1],
+            'goal_size': 1.414,
+            'goal_keepout': 1.414,
+            'goal_locations': [(1, 1)],
+            'robot_keepout': 1.414,
+            'robot_locations': [(-1, -1)],
+            'robot_rot': np.sin(np.pi / 4),
+            'terminate_resample_failure': True,
+            '_seed': 0,
+        }
+        env = Engine(config)
+        env.reset()
+        self.assertEqual(env.steps, 0)
