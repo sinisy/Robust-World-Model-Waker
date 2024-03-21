596b9f1aa37471d7c5a7c806a84b502d3964b4bd
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-21 06:40:45 +0000

    Update lss-ffinningen.user.js

--- waker/envs/wrappers.py
+++ waker/envs/wrappers.py
@@ -11,4 +11,23 @@ import common
 
 class SafetyGymWrapper:
 
-  def __init__(self, name, env, action_repeat=1, 
+  def __init__(self, name, env, action_repeat=1, obs_key='state', act_key='action', size=(64, 64)):
+    if "all" in name:
+      self._dict_reward = True
+      self._tasks = common.DOMAIN_TASK_IDS[name]
+    self.name = name
+
+    self._env = env
+    self._obs_is_dict = hasattr(self._env.observation_space, 'spaces')
+    self._act_is_dict = hasattr(self._env.action_space, 'spaces')
+    self._obs_key = obs_key
+    self._act_key = act_key
+    self._action_repeat = action_repeat
+    self._size = size
+
+  def __getattr__(self, name):
+    if name.startswith('__'):
+      raise AttributeError(name)
+    try:
+      return getattr(self._env, name)
+    except AttributeError
