4f055ac69e67aa12cf4a944002e6df01783a883b
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-21 10:26:37 +0000

    Sprememba vsebine začetne datoteke

--- waker/envs/wrappers.py
+++ waker/envs/wrappers.py
@@ -30,4 +30,24 @@ class SafetyGymWrapper:
       raise AttributeError(name)
     try:
       return getattr(self._env, name)
-    except AttributeError
+    except AttributeError:
+      raise ValueError(name)
+
+  @property
+  def obs_space(self):
+    if self._obs_is_dict:
+      spaces = self._env.observation_space.spaces.copy()
+    else:
+      spaces = {self._obs_key: self._env.observation_space}
+    return {
+        **spaces,
+        'image': gym.spaces.Box(0, 255, self._size + (3,), dtype=np.uint8),
+        'reward': gym.spaces.Box(-np.inf, np.inf, (), dtype=np.float32),
+        'is_first': gym.spaces.Box(0, 1, (), dtype=bool),
+        'is_last': gym.spaces.Box(0, 1, (), dtype=bool),
+        'is_terminal': gym.spaces.Box(0, 1, (), dtype=bool),
+    }
+
+  @property
+  def act_space(self):
+    return {self._act_key
