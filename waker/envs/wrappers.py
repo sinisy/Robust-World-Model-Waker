c2cc10b19a1e8555ab811b4fef56dd43f4dede7d
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-25 09:12:26 +0000

    Update estilos.css

--- waker/envs/wrappers.py
+++ waker/envs/wrappers.py
@@ -272,4 +272,20 @@ class CombinedEnvWrapper:
 class DMC:
 
   def __init__(self, name, action_repeat=1, size=(64, 64), camera=None):
-    domain, task = name.spli
+    domain, task = name.split('_', 1)
+    if task == 'all':
+        self._dict_reward = True
+        self._tasks = common.DOMAIN_TASK_IDS[name]
+    else:
+        self._dict_reward = False
+    self.name = name
+    if domain == 'cup':  # Only domain with multiple words.
+      domain = 'ball_in_cup'
+    if domain == 'manip':
+      from dm_control import manipulation
+      self._env = manipulation.load(task + '_vision')
+    elif domain == 'locom':
+      from dm_control.locomotion.examples import basic_rodent_2020
+      self._env = getattr(basic_rodent_2020, task)()
+    else:
+      from envs.dm_control import sui
