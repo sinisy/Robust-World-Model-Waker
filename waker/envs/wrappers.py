7539f3fe274b2dd75445b0213c7316a43804bf83
sinisy <136992380+sinisy@users.noreply.github.com>
2024-03-27 17:38:17 +0000

    Bump Go version

--- waker/envs/wrappers.py
+++ waker/envs/wrappers.py
@@ -503,4 +503,25 @@ class OneHotAction:
     space = gym.spaces.Box(low=0, high=1, shape=shape, dtype=np.float32)
     space.sample = self._sample_action
     space.n = shape[0]
-    return {**self._env.act_space, self._
+    return {**self._env.act_space, self._key: space}
+
+  def step(self, action):
+    index = np.argmax(action[self._key]).astype(int)
+    reference = np.zeros_like(action[self._key])
+    reference[index] = 1
+    if not np.allclose(reference, action[self._key]):
+      raise ValueError(f'Invalid one-hot action:\n{action}')
+    return self._env.step({**action, self._key: index})
+
+  def reset(self):
+    return self._env.reset()
+
+  def _sample_action(self):
+    actions = self._env.act_space.n
+    index = self._random.randint(0, actions)
+    reference = np.zeros(actions, dtype=np.float32)
+    reference[index] = 1.0
+    return reference
+
+
+class Re
